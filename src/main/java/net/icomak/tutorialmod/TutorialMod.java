2ccc069f6d2dfd0168c5b8e7dbc72a0a43ae0c24
IcoMak <federicomattioli501@gmail.com>
2024-06-26 17:23:51 +0200

    Initial Commit

---
+++ src/main/java/net/icomak/tutorialmod/TutorialMod.java
@@ -0,0 +1,17 @@
+package net.icomak.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorial-mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
